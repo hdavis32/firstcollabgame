2f992983d84758e4d521e78aa7e40d15e5f6dae8
hdavis32 <1424842@sd61learn.ca>
2024-10-08 14:12:54 -0700

    .

--- spawner.gd
+++ spawner.gd
@@ -1,19 +1,17 @@
 extends Node2D
 
-var collectable = preload("res://gem.tscn")
+var collectable = preload("res://collectable.tscn")
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(delta: float) -> void:
 	pass
 
 
-func _on_timer_timeout():
+func _on_timer_timeout() -> void:
 	var newcollectable = collectable.instantiate()
 	add_child(newcollectable)
-	var spawnpos = randi_range(0,get_window().content_scale_size.x)
-	newcollectable.position.x = spawnpos
-	
+	newcollectable.position.x = randi_range(0,get_viewport().size.x)
