9a34be366f734c4d59e6e362b0ee86cb7e883b56
hdavis32 <1424842@sd61learn.ca>
2024-10-10 14:08:14 -0700

    fix

--- astroidspawner.gd
+++ astroidspawner.gd
@@ -1,16 +1,17 @@
 extends Node2D
 
-var astroid = preload("res://astroid.tscn")
+var collectable = preload("res://astroid.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 
-func _on_timer_2_timeout() -> void:
-	var newastroid = astroid.instantiate()
-	add_child(newastroid)
-	newastroid.position.x = randi_range(0,get_viewport().size.x)
+func _on_timer_timeout() -> void:
+	var newcollectable = collectable.instantiate()
+	add_child(newcollectable)
+	newcollectable.position.x = randi_range(0,get_viewport().size.x)
