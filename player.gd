458d8b66ad0686059d578c4d670dbb8ad98e8321
hdavis32 <1424842@sd61learn.ca>
2024-10-11 12:13:24 -0700

    movement

--- player.gd
+++ player.gd
@@ -16,10 +16,10 @@ func _process(delta):
 	else:
 		velocity.x = 0
 	if Input.is_action_pressed("jump"):
-		velocity.y = -450
-	else: 
-		velocity.y = 150
-	if Input.is_action_pressed("crouch"):
+		velocity.y = -movespeed
+	elif Input.is_action_pressed("crouch"):
 		velocity.y = movespeed
+	else:
+		velocity.y = 0
 			
 	move_and_slide()
